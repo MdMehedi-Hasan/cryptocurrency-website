821f9921d197bf6e043049a1cb8903208e43d02c
joy1954islam <joyislam1954@gmail.com>
2022-10-31 10:30:45 +0600

    d

--- Assets/script.js
+++ Assets/script.js
@@ -1,13 +1,16 @@
+// ========================= Api calling for calculating price of bitcoin ======================
 var apikey = {
     key:'1000b77a-d7fb-42c3-a6ff-83e3627886cf',
 };
 fetch('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=' + apikey.key)
 .then((response) => response.json())
 .then((data) => console.log(data));
+// ========================= Api calling end ======================
+
 
 // ================= Box countdown ==========================
 // Set the date we're counting down to
-let countDownDate = new Date("Jan 5, 2024 15:37:25").getTime();
+let countDownDate = new Date("Jan 5, 2023 15:37:25").getTime();
 
 // Update the count down every 1 second
 const x = setInterval(function() {
